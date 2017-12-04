e350554fcda4e6e733e37052047ac77b4416dd83
Ryan <johnson2500@live.com>
2017-12-03 19:24:10 -0800

    Remove Files
    Removing Unused files

--- React-gameStick/backend/gui.py
+++
@@ -1,4 +0,0 @@
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWebKitWidgets import QWebView
-import sys
--- React-gameStick/socketServer/games.js
+++ React-gameStick/socketServer/games.js
@@ -1,21 +1,3 @@
-class Game {
-    constructor() {
-        this.decks = [];
-        this.users = [];
-        this.isPicking = {};
-        this.cardsOnTable = [];
-    }
-    addUser(user) {
-        this.users.push(user);
-    }
-    isPickingNow(user) {
-        this.isPicking.userName = user.userName;
-        this.isPicking.socketID = user.socketID;
-    }
-    addCardToTable(card) {
-        this.cardsOnTable.push(card);
-    }
-
-}
-
-module.exports = Game
+//==============================================================================
+//=== File for Games ===========================================================
+//==============================================================================
