f4ef12d4c075afc135c4cb25a3eb367fcecad54e
Ryan <johnson2500@live.com>
2018-01-07 20:36:03 -0800

    Junk

--- server/socket/socketServer.js
+++ server/socket/socketServer.js
@@ -3,6 +3,7 @@ var io = require('socket.io')(4000);
 console.log("Socket Server Running on 4000!");
 
 io.on('connection',(socket)=>{ // Generic "/" route which everyone hits first
+
   // Logic for joining room
 
   // Logic for starting room
